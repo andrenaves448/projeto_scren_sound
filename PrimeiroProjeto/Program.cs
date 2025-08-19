d36e737742ad7e60d73971e07b0e42e731ec644a
André <andrenaves448@gmail.com>
2025-08-18 21:45:09 -0300

    feat: exibe mensagem de boas-vindas estilizada e opções do menu principal

--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -3,9 +3,28 @@ string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
 
 void ExibirMensagemDeBoasVindas()
 {
-    Console.WriteLine("***************************");
+    //fsymbols.com
+    Console.WriteLine(@"
+
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+
+");
+    //Console.WriteLine("**************************************************************************");
     Console.WriteLine(mensagemDeBoasVindas);
-    Console.WriteLine("***************************");
+}
+
+void ExibirOpcoesDoMenu() 
+{
+    Console.WriteLine("Digite 1 para registrar uma banca");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda");
+    Console.WriteLine("Digite -1 para sair");
 }
 
 ExibirMensagemDeBoasVindas();
