b8c3f888d3a2b271024da4eff640b5728064f2a9
André <andrenaves448@gmail.com>
2025-08-19 18:01:34 -0300

    Alteração do nome da functio ExibirMensagemDeBoasVindas() para  ExibirLogo()e atribui ao ExibirOpcoesDoMenu()

--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Screen Sound                
 string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
 
-void ExibirMensagemDeBoasVindas()
+void ExibirLogo()
 {
 
     //fsymbols.com
@@ -21,6 +21,7 @@ void ExibirMensagemDeBoasVindas()
 
 void ExibirOpcoesDoMenu() 
 {
+    ExibirLogo();
     Console.WriteLine("\nDigite 1 para registrar uma banca");
     Console.WriteLine("Digite 2 para mostrar todas as bandas");
     Console.WriteLine("Digite 3 para avaliar uma banda");
@@ -63,5 +64,5 @@ void RegistrarBanda()
     ExibirOpcoesDoMenu();
 }
 
-ExibirMensagemDeBoasVindas();
+
 ExibirOpcoesDoMenu();
