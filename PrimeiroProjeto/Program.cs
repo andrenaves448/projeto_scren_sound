747a507c395a3a4831da53911bd36086497f3496
André <andrenaves448@gmail.com>
2025-08-19 00:01:55 -0300

    feat: adiciona leitura e tratamento da opção do menu principal

--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -3,6 +3,7 @@ string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
 
 void ExibirMensagemDeBoasVindas()
 {
+
     //fsymbols.com
     Console.WriteLine(@"
 
@@ -20,11 +21,20 @@ void ExibirMensagemDeBoasVindas()
 
 void ExibirOpcoesDoMenu() 
 {
-    Console.WriteLine("Digite 1 para registrar uma banca");
+    Console.WriteLine("\nDigite 1 para registrar uma banca");
     Console.WriteLine("Digite 2 para mostrar todas as bandas");
     Console.WriteLine("Digite 3 para avaliar uma banda");
     Console.WriteLine("Digite 4 para exibir a média de uma banda");
     Console.WriteLine("Digite -1 para sair");
+
+    Console.Write("\nDigite a opção desejada: ");
+    string opcaoEscolhida= Console.ReadLine()!;
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+    if (opcaoEscolhidaNumerica == 1) 
+    {
+        Console.WriteLine("Você digitou a opção " + opcaoEscolhida);
+    }
 }
 
 ExibirMensagemDeBoasVindas();
+ExibirOpcoesDoMenu();
