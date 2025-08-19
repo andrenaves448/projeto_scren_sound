3e0b46067d2298f0dd3dbcaec4add46967c10818
André <andrenaves448@gmail.com>
2025-08-18 21:25:54 -0300

    Adicionar arquivos de projeto.

---
+++ PrimeiroProjeto/Program.cs
@@ -0,0 +1,11 @@
+﻿// Screen Sound                
+string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
+
+void ExibirMensagemDeBoasVindas()
+{
+    Console.WriteLine("***************************");
+    Console.WriteLine(mensagemDeBoasVindas);
+    Console.WriteLine("***************************");
+}
+
+ExibirMensagemDeBoasVindas();
